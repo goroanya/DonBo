e75e7305ec246b41277d2801750e40352d56f1f5
Anna Zavhorodnia <Anna_Zavhorodnia@epam.com>
2019-12-17 14:16:17 +0200

    add some templates

--- web/src/main/java/com/donbo/controller/PageController.java
+++ web/src/main/java/com/donbo/controller/PageController.java
@@ -1,13 +1,21 @@
 package com.donbo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
 
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("module","index");
+        return "index";
+    }
+
     @GetMapping("/all_appointments")
-    public String allAppointmentsPage(){
+    public String allAppointmentsPage(Model model){
+        model.addAttribute("module", "all_appointments");
         return "allAppointments";
     }
 
